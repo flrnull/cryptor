76ad7e7430ff5c0aa990f3b04e6a1b9163839f39
Evgeniy Udodov <flr.null@gmail.com>
2024-12-31 15:46:13 +0300

    progress

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,43 +1,9 @@
-import './style.css';
-import './app.css';
-
 import logo from './assets/images/logo-universal.png';
-import {Greet} from '../wailsjs/go/main/App';
 
 document.querySelector('#app').innerHTML = `
-    <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
-      <div class="input-box" id="input">
-        <input class="input" id="name" type="text" autocomplete="off" />
-        <button class="btn" onclick="greet()">Greet</button>
-      </div>
-    </div>
+  <div>
+    <img src="${logo}" alt="Logo" />
+    <h1>Welcome to Cryptor!</h1>
+    <p>Use the app to encrypt or decrypt your files.</p>
+  </div>
 `;
-document.getElementById('logo').src = logo;
-
-let nameElement = document.getElementById("name");
-nameElement.focus();
-let resultElement = document.getElementById("result");
-
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement.value;
-
-    // Check if the input is empty
-    if (name === "") return;
-
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
-    }
-};
